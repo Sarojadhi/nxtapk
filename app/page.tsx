69e944ff19b13a6b81ad2c45d248a92b6e420972
Saroj Adhikari <sarojadh1111@gmail.com>
2025-12-12 08:31:12 +0545

    data fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
-import React from "react";
-// import Link from "next/link";
-import Btn from "./component/Btn";
+import React from 'react'
 
-export default function Page() {
+interface User {
+  id: number;
+  username: string;
+}
+
+const Page = async () => {
+  const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await resp.json();
+
   return (
     <>
-      <Btn /> 
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+        Users
+      </h1>
+
+      <ul style={{ 
+        listStyle: "none",
+        padding: 0,
+        maxWidth: "300px",
+        margin: "0 auto"
+      }}>
+        {users.map(user => (
+          <li 
+            key={user.id}
+            style={{ 
+              padding: "10px",
+              border: "1px solid #ddd",
+              borderRadius: "6px",
+              marginBottom: "8px",
+              background: "#f9f9f9",
+              color:"black"
+            }}
+          >
+            {user.username}
+          </li>
+        ))}
+      </ul>
     </>
-  );
+  )
 }
+
+export default Page;
