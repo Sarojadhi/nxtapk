9e5281881de36d31597ddcf13781ebbbbbdf42b2
Saroj Adhikari <sarojadh1111@gmail.com>
2025-12-12 08:43:49 +0545

    cacheing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 interface User {
   id: number;
@@ -6,39 +6,55 @@ interface User {
 }
 
 const Page = async () => {
-  const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+  const resp = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-cache",
+  });
+  // no -cache lw node js lw sadai naya data fetch garxa .
+
+  /*  cache: "force-cache" → default (static data)
+
+  cache: "no-cache" → always fresh
+  
+ cache: "no-store" → never cache, full dynamic */
+
   const users: User[] = await resp.json();
 
   return (
     <>
-      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
-        Users
-      </h1>
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Users</h1>
 
-      <ul style={{ 
-        listStyle: "none",
-        padding: 0,
-        maxWidth: "300px",
-        margin: "0 auto"
-      }}>
-        {users.map(user => (
-          <li 
+      <ul
+        style={{
+          listStyle: "none",
+          padding: 0,
+          maxWidth: "300px",
+          margin: "0 auto",
+        }}
+      >
+        {users.map((user) => (
+          <li
             key={user.id}
-            style={{ 
+            style={{
               padding: "10px",
               border: "1px solid #ddd",
               borderRadius: "6px",
               marginBottom: "8px",
               background: "#f9f9f9",
-              color:"black"
+              color: "black",
             }}
           >
             {user.username}
           </li>
         ))}
       </ul>
+
+      {/* <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul> */}
     </>
-  )
-}
+  );
+};
 
 export default Page;
